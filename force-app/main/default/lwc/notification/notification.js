acf1c7b00408bdb2a42a5a7dcbe9433c2dc5b25b
Daniel Probert <danielbprobert@gmail.com>
2022-06-16 17:07:29 +0100

    update to include soft validation process

--- force-app/main/default/lwc/notification/notification.js
+++ force-app/main/default/lwc/notification/notification.js
@@ -2,55 +2,86 @@
  * @description       : 
  * @author            : daniel@hyphen8.com
  * @group             : 
- * @last modified on  : 01-08-2021
+ * @last modified on  : 16/06/2022
  * @last modified by  : daniel@hyphen8.com
  * Modifications Log 
  * Ver   Date         Author               Modification
  * 1.0   01-08-2021   daniel@hyphen8.com   Initial Version
 **/
-import { LightningElement, track, api } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
 
 import getNotificationMessage from '@salesforce/apex/NotificationController.getMessage';
+import { getRecord } from 'lightning/uiRecordApi';
+
 
 export default class Notification extends LightningElement {
 
-    // setup api elements
     @api recordId;
     @api objectApiName;
     @api notificationTitle;
     @api fieldAPIName;
-    @api componentStyling;
+    @api notificationStyling;
+    @api notificationMessage;
+    @api notificationCriteria;
+    @api notificationIcon;
 
-    // setup track elements
+
     message;
-    errors;
     hasMessages;
 
-    // get our message to display
+    get scopedVariant() {
+        if(this.notificationStyling == 'slds-theme_success'){
+            return 'inverse';
+        } else if(this.notificationStyling == 'slds-theme_warning'){
+            return 'warning';
+        } else if(this.notificationStyling == 'slds-theme_error'){
+            return 'inverse';
+        } else if(this.notificationStyling == 'slds-theme_info'){
+            return 'inverse';
+        } else {
+            return '';
+        }
+    }
+
+    // Wire function that is used to detect changes on the record and rerun the get messages method
+    @wire(getRecord, { recordId: '$recordId', layoutTypes: ['Full']})
+    getCurrentRecord({ data, error }) {
+        if (data) {
+            this.handleGetMessages();
+        } else if (error) {
+            console.error('Wire Error Encountered => ', JSON.stringify(error));
+        }
+    };
+
+    // function to get our message to display
     handleGetMessages() {
         getNotificationMessage({
             recordId: this.recordId,
             objectName: this.objectApiName,
-            fieldAPIName: this.fieldAPIName
+            fieldAPIName: this.fieldAPIName,
+            whereClause: this.notificationCriteria
         })
         .then((results) => {
-            this.message = results;
+            if(this.notificationCriteria != null){
+                this.message = this.notificationMessage;
+            } else {
+                this.message = results;
+            }
             if(results.length > 0){
                 this.hasMessages = true;
             } else {
                 this.hasMessages = false;
             }
-            this.errors = undefined;  
         })
         .catch((error) => {
-            this.errors = JSON.stringify(error);
+            console.error('handleGetMessages error > ' + JSON.stringify(error));
             this.messages = undefined;
         });
     }
 
     // get the styling for the alert based on the property
     get alertStyling() {
-        return 'slds-notify slds-p-around_medium ' + this.componentStyling;
+        return 'slds-scoped-notification slds-media slds-media_center slds-is-relative slds-var-p-around_medium ' + this.notificationStyling;
     }
 
     // intial call back get out data
